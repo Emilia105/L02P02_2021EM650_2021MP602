70c844e2e1d964fa37bf16f92049df7e30b33037
Rickmonte2022 <ricardo.monterrosa@catolica.edu.sv>
2024-04-20 21:43:57 -0600

    maquetado proto 2

--- L02P02_2021EM650_2021MP602/Controllers/LibrosController.cs
+++ L02P02_2021EM650_2021MP602/Controllers/LibrosController.cs
@@ -1,5 +1,6 @@
 ﻿using L02P02_2021EM650_2021MP602.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace L02P02_2021EM650_2021MP602.Controllers
 {
@@ -13,7 +14,21 @@ namespace L02P02_2021EM650_2021MP602.Controllers
         }
         public IActionResult Index()
         {
+
+            string nombreAutor = "PEPE";
+            var libros = (from l in _librosContext.Libros
+                          join a in _librosContext.Autores on l.IdAutor equals a.Id
+                          
+                          select new
+                          {
+                              id = l.Id,
+                              nombre = l.Nombre
+                          }).ToList();
+
+            ViewData["autor"] = nombreAutor;
+            ViewData["libros"] = libros;
             return View();
+
         }
     }
 }
