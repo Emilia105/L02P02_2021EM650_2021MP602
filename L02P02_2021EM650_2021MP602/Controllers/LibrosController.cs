d441c9fec113a15ee0f767b82fac5b93ef8e50b6
2021EM650@unicaeselsalvador0.onmicrosoft.com <2021EM650@unicaeselsalvador0.onmicrosoft.com>
2024-04-21 05:03:37 -0600

    Arreglos de vista y metodo para agregar al carrito

--- L02P02_2021EM650_2021MP602/Controllers/LibrosController.cs
+++ L02P02_2021EM650_2021MP602/Controllers/LibrosController.cs
@@ -1,6 +1,7 @@
 ﻿using L02P02_2021EM650_2021MP602.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace L02P02_2021EM650_2021MP602.Controllers
 {
@@ -15,14 +16,16 @@ namespace L02P02_2021EM650_2021MP602.Controllers
         public IActionResult Index()
         {
 
-            string nombreAutor = "PEPE";
+            string nombreAutor = "Autor";
             var libros = (from l in _librosContext.Libros
                           join a in _librosContext.Autores on l.IdAutor equals a.Id
-                          
+
                           select new
                           {
                               id = l.Id,
-                              nombre = l.Nombre
+                              nombre = l.Nombre,
+                              nombreAutor = a.Autor,
+                              precioL = l.Precio
                           }).ToList();
 
             ViewData["autor"] = nombreAutor;
@@ -30,5 +33,33 @@ namespace L02P02_2021EM650_2021MP602.Controllers
             return View();
 
         }
-    }
+        public IActionResult AgregarLibros(int IdPedido, int IdLibro)
+        {
+            // Obtener el pedido encabezado
+            var pedidoEncabezado = _librosContext.PedidoEncabezados.Find(IdPedido);
+            if (pedidoEncabezado == null)
+            {
+                return NotFound("valiendo");
+            }
+            // Obtener el libro
+            var libro = _librosContext.Libros.Find(IdLibro);
+            if (libro == null)
+            {
+                return NotFound("madre");
+            }
+
+            // Crear un nuevo detalle de pedido
+            PedidoDetalle detalle = new PedidoDetalle
+            {
+                Id = IdPedido,
+                IdLibro = IdLibro
+            };
+
+            _librosContext.PedidoDetalles.Add(detalle);
+            _librosContext.SaveChanges();
+
+
+            return View("Index");
+        }
+    }    
 }
