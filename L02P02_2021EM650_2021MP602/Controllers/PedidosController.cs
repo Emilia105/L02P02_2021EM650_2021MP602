4db22c074adb0d46e33782777b658263ffebae9c
2021EM650@unicaeselsalvador0.onmicrosoft.com <2021EM650@unicaeselsalvador0.onmicrosoft.com>
2024-04-20 22:01:01 -0600

    Controlador y vista de pedidos

---
+++ L02P02_2021EM650_2021MP602/Controllers/PedidosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021EM650_2021MP602.Controllers
+{
+    public class PedidosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
